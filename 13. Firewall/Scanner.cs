abc501eb64526485bae7ca55f571dcad5b522e68
payou42 <34344231+payou42@users.noreply.github.com>
2017-12-13 08:51:32 +0100

    Day 13

---
+++ 13. Firewall/Scanner.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace _13._Firewall
+{
+    class Scanner
+    {
+        public int Range { get; }
+
+        public int Depth { get; }
+
+        public int Severity
+        {
+            get
+            {
+                return Depth * Range;
+            }
+        }
+
+        public Scanner(int depth, int range)
+        {
+            Depth = depth;
+            Range = range;
+        }
+
+        public bool IsScanned(int start)
+        {
+            return (start + Depth) % (2 * Range - 2) == 0;
+        }
+    }
+}
