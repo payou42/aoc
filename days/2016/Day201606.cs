9f8c70be3157a24907e1973b9d37660fed3efb6e
payou <alexandre.perrin@ubisoft.com>
2017-12-27 09:11:48 +0100

    Add days 6 & 7 of 2016

---
+++ days/2016/Day201606.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+using Aoc.Common;
+
+namespace Aoc
+{
+    public class Day201606 : Aoc.Framework.Day
+    {
+        public string Codename { get; private set; }
+
+        public string Name { get; private set; }
+
+        private string[] _messages;
+
+        public Day201606()
+        {
+            Codename = "2016-06";
+            Name = "Signals and Noise";
+        }
+
+        public void Init()
+        {
+            _messages = Aoc.Framework.Input.GetStringVector(this, "\n");
+        }
+
+        public string Run(Aoc.Framework.Part part)
+        {
+            string decoded = "";
+            for (int j = 0; j < _messages[0].Length; ++j)
+            {
+                Registers counter = new Registers();
+                foreach (string s in _messages)
+                {
+                    counter[s[j].ToString()]++;
+                }
+                Int64 check =  (part == Aoc.Framework.Part.Part1) ? counter.Storage.Values.Max() : counter.Storage.Values.Min();
+                decoded += counter.Storage.Where(pair => check.Equals(pair.Value)).Select(pair => pair.Key).First();
+            }
+            return decoded;
+        }
+    }   
+}
---
+++ days/2016/Day201607.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+using Aoc.Common;
+
+namespace Aoc
+{
+    public class Day201607 : Aoc.Framework.Day
+    {
+        public string Codename { get; private set; }
+
+        public string Name { get; private set; }
+
+        private string[] _ips;
+
+        public Day201607()
+        {
+            Codename = "2016-07";
+            Name = "Internet Protocol Version 7";
+        }
+
+        public void Init()
+        {
+            _ips = Aoc.Framework.Input.GetStringVector(this, "\n");
+        }
+
+        public string Run(Aoc.Framework.Part part)
+        {
+            if (part == Aoc.Framework.Part.Part1)
+            {
+                return _ips.Where(ip => HasTLS(ip)).Count().ToString();
+            }
+
+            if (part == Aoc.Framework.Part.Part2)
+            {
+                return _ips.Where(ip => HasSSL(ip)).Count().ToString();
+            }
+
+            return "";
+        }
+
+        private bool HasTLS(string ip)
+        {
+            bool inside = false;
+            bool hasAbba = false;
+            for (int i = 0; i < ip.Length - 3; ++i)
+            {
+                if (ip[i] == '[') inside = true;
+                if (ip[i] == ']') inside = false;
+                if (ip[i + 1] != '[' && ip[i + 1] != ']' && ip[i + 2] == ip[i + 1] && ip[i + 3] == ip[i] && ip[i] != ip [i + 1])
+                {
+                    if (inside) return false;
+                    hasAbba = true;
+                }
+            }
+            return hasAbba;
+        }
+
+        private bool HasSSL(string ip)
+        {
+            bool inside = false;
+            List<string> abas = new List<string>();
+            List<string> babs = new List<string>();
+
+            for (int i = 0; i < ip.Length - 2; ++i)
+            {
+                if (ip[i] == '[') inside = true;
+                if (ip[i] == ']') inside = false;
+                if (ip[i + 1] != '[' && ip[i + 1] != ']' && ip[i + 1] != ip[i] && ip[i] == ip[i + 2])
+                {
+                    if (inside)
+                    {
+                        babs.Add(ip.Substring(i, 3));
+                    }
+                    else
+                    {
+                        abas.Add(ip.Substring(i, 3));
+                    }
+                }
+            }
+
+            foreach (string aba in abas)
+            {
+                string bab = new String(new Char[3] { aba[1], aba[0], aba[1] });
+                if (babs.Contains(bab))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }   
+}
