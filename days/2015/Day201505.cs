6148cae8fc5955bd8464a93c1c865c77463c26f8
payou <alexandre.perrin@ubisoft.com>
2018-12-05 08:25:52 +0100

    Day 5 of 2015

--- days/2015/Day201505.cs
+++ days/2015/Day201505.cs
@@ -3,6 +3,7 @@ using System.Text;
 using System.Linq;
 using System.Collections.Generic;
 using Aoc.Common;
+using Aoc.Common.Strings;
 
 namespace Aoc
 {
@@ -12,6 +13,8 @@ namespace Aoc
 
         public string Name { get; private set; }
 
+        private string[] _input;
+
         public Day201505()
         {
             Codename = "2015-05";
@@ -20,18 +23,68 @@ namespace Aoc
 
         public void Init()
         {
+            _input = Aoc.Framework.Input.GetStringVector(this);
         }
 
         public string Run(Aoc.Framework.Part part)
         {
             if (part == Aoc.Framework.Part.Part1)
             {
-                return "part1";
+                long count = 0;
+                string[] forbidden = new string[] { "ab", "cd", "pq", "xy" };
+                foreach (string s in _input)
+                {
+                    int voyels = s.ToCharArray().Count(c => "aeiou".Contains(c));
+                    if (voyels < 3)
+                    {
+                        continue;
+                    }
+
+                    char? repeated = Repetition.First(s, 2);
+                    if (repeated == null)
+                    {
+                        continue;
+                    }
+
+                    bool wrong = false;
+                    foreach (string f in forbidden)
+                    {
+                        if (s.Contains(f))
+                        {
+                            wrong = true;
+                            break;
+                        }
+                    }
+                    if (wrong)
+                    {
+                        continue;
+                    }
+
+                    count++;
+                }
+
+                return count.ToString();
             }
 
             if (part == Aoc.Framework.Part.Part2)
             {
-                return "part2";
+                long count = 0;
+                foreach (string s in _input)
+                {
+                    if (!Repetition.Group(s, 2))
+                    {
+                        continue;
+                    }
+
+                    if (!Repetition.Spaced(s, 1))
+                    {
+                        continue;
+                    }
+
+                    count++;
+                }
+
+                return count.ToString();
             }
 
             return "";
