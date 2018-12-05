9da6eb912e9c651343f2a0a1a6ab34de4bae338f
payou <alexandre.perrin@ubisoft.com>
2018-12-05 08:04:53 +0100

    Day 4 of 2015

--- days/2015/Day201504.cs
+++ days/2015/Day201504.cs
@@ -3,6 +3,7 @@ using System.Text;
 using System.Linq;
 using System.Collections.Generic;
 using Aoc.Common;
+using Aoc.Common.Crypto;
 
 namespace Aoc
 {
@@ -12,6 +13,8 @@ namespace Aoc
 
         public string Name { get; private set; }
 
+        private string _input;
+
         public Day201504()
         {
             Codename = "2015-04";
@@ -20,18 +23,29 @@ namespace Aoc
 
         public void Init()
         {
+            _input = Aoc.Framework.Input.GetString(this);
         }
 
         public string Run(Aoc.Framework.Part part)
         {
             if (part == Aoc.Framework.Part.Part1)
             {
-                return "part1";
+                long i = 1;
+                while (!Md5.Compute(_input, i).StartsWith("00000"))
+                {
+                    i++;
+                }
+                return i.ToString();
             }
 
             if (part == Aoc.Framework.Part.Part2)
             {
-                return "part2";
+                long i = 1;
+                while (!Md5.Compute(_input, i).StartsWith("000000"))
+                {
+                    i++;
+                }
+                return i.ToString();
             }
 
             return "";
