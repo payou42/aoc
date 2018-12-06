29402291ef93bf2c4b8d655d23dc8dfbe6d6203e
UBISOFT-ORG\alexandre.perrin <alexandre.perrin@ubisoft.com>
2018-12-06 12:36:47 +0100

    Day 10 of 2015

--- days/2015/Day201510.cs
+++ days/2015/Day201510.cs
@@ -12,6 +12,8 @@ namespace Aoc
 
         public string Name { get; private set; }
 
+        private string _input;
+
         public Day201510()
         {
             Codename = "2015-10";
@@ -20,21 +22,57 @@ namespace Aoc
 
         public void Init()
         {
+            _input = Aoc.Framework.Input.GetString(this);
         }
 
         public string Run(Aoc.Framework.Part part)
         {
             if (part == Aoc.Framework.Part.Part1)
             {
-                return "part1";
+                return LookAndSay(_input, 40).ToString();
             }
 
             if (part == Aoc.Framework.Part.Part2)
             {
-                return "part2";
+                return LookAndSay(_input, 50).ToString();
             }
 
             return "";
+        }
+
+        private long LookAndSay(string input, int iterations)
+        {
+            // Prepare the data
+            List<int> current = input.Select(c => int.Parse(c.ToString())).ToList();
+
+            // Iterate as many times as required
+            for (int i = 0; i < iterations; ++i)
+            {
+                int previous = 0;
+                int count = 0;
+                List<int> next = new List<int>();
+
+                foreach (int n in current)
+                {
+                    if ((previous == 0) || (previous == n))
+                    {
+                        previous = n;
+                        count++;
+                    }
+                    else
+                    {
+                        next.Add(count);
+                        next.Add(previous);
+                        previous = n;
+                        count = 1;
+                    }
+                }
+                next.Add(count);
+                next.Add(previous);
+                current = next;
+            }
+
+            return current.Count;
         }
     }   
 }
