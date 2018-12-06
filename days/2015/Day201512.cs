76c476155340b3000f4206c3202e95f195ab2bd3
UBISOFT-ORG\alexandre.perrin <alexandre.perrin@ubisoft.com>
2018-12-06 13:24:16 +0100

    Day 12 of 2015

--- days/2015/Day201512.cs
+++ days/2015/Day201512.cs
@@ -2,6 +2,8 @@ using System;
 using System.Text;
 using System.Linq;
 using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Aoc.Common;
 
 namespace Aoc
@@ -12,6 +14,8 @@ namespace Aoc
 
         public string Name { get; private set; }
 
+        private string _input;
+
         public Day201512()
         {
             Codename = "2015-12";
@@ -20,21 +24,67 @@ namespace Aoc
 
         public void Init()
         {
+            _input = Aoc.Framework.Input.GetString(this);
         }
 
         public string Run(Aoc.Framework.Part part)
         {
             if (part == Aoc.Framework.Part.Part1)
             {
-                return "part1";
+                // Load the content
+                dynamic content = JsonConvert.DeserializeObject(_input);
+
+                // Sum all of the JToken
+                return GetJsonSum(content.Root, false).ToString();
             }
 
             if (part == Aoc.Framework.Part.Part2)
             {
-                return "part2";
+                // Load the content
+                dynamic content = JsonConvert.DeserializeObject(_input);
+
+                // Sum all of the JToken
+                return GetJsonSum(content.Root, true).ToString();
             }
 
             return "";
+        }
+
+        private long GetJsonSum(JToken node, bool excludeRed)
+        {
+            if (node.Type == JTokenType.Integer)
+            {
+                return (long)node;
+            }
+            else
+            {
+                long sum = 0;
+
+                if (excludeRed && node.Type == JTokenType.Object)
+                {
+                    foreach (JToken child in node.Children())
+                    {
+                        if (child.Type == JTokenType.Property)
+                        {
+                            foreach (JToken v in child.Children())
+                            {
+                                if (v.Type == JTokenType.String)
+                                {
+                                    if ((string)v == "red")
+                                    {
+                                        return 0;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                foreach (JToken child in node.Children())
+                {
+                    sum += GetJsonSum(child, excludeRed);
+                }
+                return sum;
+            }
         }
     }   
 }
