fa693aec295ac3822031407260edfc5e49c9de31
UBISOFT-ORG\alexandre.perrin <alexandre.perrin@ubisoft.com>
2018-12-06 11:45:53 +0100

    Day 8 of 2015

--- days/2015/Day201508.cs
+++ days/2015/Day201508.cs
@@ -3,6 +3,7 @@ using System.Text;
 using System.Linq;
 using System.Collections.Generic;
 using Aoc.Common;
+using System.Text.RegularExpressions;
 
 namespace Aoc
 {
@@ -12,6 +13,8 @@ namespace Aoc
 
         public string Name { get; private set; }
 
+        private string[] _input;
+
         public Day201508()
         {
             Codename = "2015-08";
@@ -20,21 +23,67 @@ namespace Aoc
 
         public void Init()
         {
+            _input = Aoc.Framework.Input.GetStringVector(this);
         }
 
         public string Run(Aoc.Framework.Part part)
         {
             if (part == Aoc.Framework.Part.Part1)
             {
-                return "part1";
+                return _input.Sum(s => 2 + (s.Length - GetUnescapedLength(s))).ToString();
             }
 
             if (part == Aoc.Framework.Part.Part2)
             {
-                return "part2";
+                return _input.Sum(s => 2 + (GetEscapedLength(s) - s.Length)).ToString();
             }
 
             return "";
+        }
+
+        private int GetUnescapedLength(string s)
+        {
+            int diff = 0;
+            Stack<char> queue = new Stack<char>();
+            char[] data = s.ToCharArray();
+            for (int i = 0; i < data.Length; ++i)
+            {
+                if ((data[i] == '"') || (data[i] == '\\') || (data[i] == 'x'))
+                {
+                    if ((queue.TryPeek(out var c)) && (c == '\\'))
+                    {
+                        queue.Pop();
+                        queue.Push('.');
+                        diff += 1;
+
+                        if (data[i] == 'x')
+                        {
+                            diff += 2;
+                            i += 2;
+                        }
+
+                        continue;
+                    }
+                }
+
+                queue.Push(data[i]);
+            }
+            
+            return s.Length - diff;
+        }
+
+        private int GetEscapedLength(string s)
+        {
+            int diff = 0;
+            char[] data = s.ToCharArray();
+            for (int i = 0; i < data.Length; ++i)
+            {
+                if ((data[i] == '"') || (data[i] == '\\'))
+                {
+                    diff++;
+                }
+            }            
+            return s.Length + diff;
         }
     }   
 }
