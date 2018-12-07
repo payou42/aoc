0fe73fda97f2e8c2dfd5c6a8c422367366530c95
UBISOFT-ORG\alexandre.perrin <alexandre.perrin@ubisoft.com>
2018-12-07 13:55:53 +0100

    Day 17 of 2015

--- days/2015/Day201517.cs
+++ days/2015/Day201517.cs
@@ -12,6 +12,8 @@ namespace Aoc
 
         public string Name { get; private set; }
 
+        private int[] _capacities;
+
         public Day201517()
         {
             Codename = "2015-17";
@@ -20,20 +22,98 @@ namespace Aoc
 
         public void Init()
         {
+            _capacities = Aoc.Framework.Input.GetIntVector(this);
+            Array.Sort(_capacities);
         }
 
         public string Run(Aoc.Framework.Part part)
         {
             if (part == Aoc.Framework.Part.Part1)
             {
-                return "part1";
+                // Solution counter
+                long count = 0;
+
+                // Initial data
+                Queue<(List<int>, List<int>)> queue = new Queue<(List<int>, List<int>)>();
+                queue.Enqueue((new List<int>(), _capacities.ToList()));
+                
+                // Test all possibilities
+                while (queue.TryDequeue(out var current))
+                {
+                    // Check current solution
+                    int liters = current.Item1.Sum();
+                    if (liters >= 150)
+                    {          
+                        if (liters == 150)
+                        {
+                            count++;
+                        }
+                        continue;
+                    }
+                    
+                    // Enqueue all other possibilities
+                    for (int i = 0; i < current.Item2.Count; ++i)
+                    {
+                        var next = current.Item1.Select(o => o).ToList();
+                        next.Add(current.Item2[i]);
+                        var remaining = current.Item2.Skip(i + 1).ToList();
+                        queue.Enqueue((next, remaining));
+                    }
+                }
+
+                // All done
+                return count.ToString();
             }
 
             if (part == Aoc.Framework.Part.Part2)
             {
-                return "part2";
-            }
+                // Solution counter
+                long count = 0;
+                long used = long.MaxValue;
 
+                // Initial data
+                Queue<(List<int>, List<int>)> queue = new Queue<(List<int>, List<int>)>();
+                queue.Enqueue((new List<int>(), _capacities.ToList()));
+                
+                // Test all possibilities
+                while (queue.TryDequeue(out var current))
+                {
+                    // Discard too long solution
+                    int l = current.Item1.Count;
+                    if (l > used)
+                    {
+                        continue;
+                    }
+
+                    // Check current solution
+                    int liters = current.Item1.Sum();
+                    if (liters >= 150)
+                    {          
+                        if (liters == 150)
+                        {
+                            if (l < used)
+                            {
+                                used = l;
+                                count = 0;
+                            }
+                            count++;
+                        }
+                        continue;
+                    }
+                    
+                    // Enqueue all other possibilities
+                    for (int i = 0; i < current.Item2.Count; ++i)
+                    {
+                        var next = current.Item1.Select(o => o).ToList();
+                        next.Add(current.Item2[i]);
+                        var remaining = current.Item2.Skip(i + 1).ToList();
+                        queue.Enqueue((next, remaining));
+                    }
+                }
+
+                // All done
+                return count.ToString();
+            }
             return "";
         }
     }   
