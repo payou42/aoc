ce5cae22228e75064d8fb0b5b965525a1b396644
UBISOFT-ORG\alexandre.perrin <alexandre.perrin@ubisoft.com>
2018-12-13 12:50:40 +0100

    Day 25 of 2015

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace Aoc
         static void Main(string[] args)
         {
             // Get the command line
-            string[] command = new string[3] { "execute", "2015", "24" };
+            string[] command = new string[3] { "execute", "2015", "25" };
             if (args.Length > 0)
             {
                 command = args;
