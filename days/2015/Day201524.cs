6ac2bbce66528cd59561e95129bac7fc23eb00b3
UBISOFT-ORG\alexandre.perrin <alexandre.perrin@ubisoft.com>
2018-12-13 12:36:24 +0100

    Day 24 of 2015

--- days/2015/Day201524.cs
+++ days/2015/Day201524.cs
@@ -12,6 +12,8 @@ namespace Aoc
 
         public string Name { get; private set; }
 
+        private int[] _gifts;
+
         public Day201524()
         {
             Codename = "2015-24";
@@ -20,21 +22,68 @@ namespace Aoc
 
         public void Init()
         {
+            _gifts = Aoc.Framework.Input.GetIntVector(this);
         }
 
         public string Run(Aoc.Framework.Part part)
         {
             if (part == Aoc.Framework.Part.Part1)
             {
-                return "part1";
+                return BestEntanglement(3).ToString();
             }
 
             if (part == Aoc.Framework.Part.Part2)
             {
-                return "part2";
+                return BestEntanglement(4).ToString();
             }
 
             return "";
+        }
+
+        private long BestEntanglement(int nbGroups)
+        {
+            // We need to divide the weights equally
+            int targetWeight = _gifts.Sum() / nbGroups;
+
+            // Find the minimum number of gifts to put in front
+            int bestCount = int.MaxValue;
+            long bestEntanglement = int.MaxValue;
+
+            // Prepare the breadth-first search
+            Queue<(List<int>, List<int>)> queue = new Queue<(List<int>, List<int>)>();
+            queue.Enqueue((new List<int>(), _gifts.Where(w => w <= targetWeight).OrderByDescending(w => w).ToList()));
+
+            // Search for the best setup
+            while (queue.TryDequeue(out var state))
+            {
+                if (state.Item1.Sum() == targetWeight)
+                {
+                    if (bestCount > state.Item1.Count)
+                    {
+                        bestCount = state.Item1.Count;
+                        bestEntanglement = state.Item1.Aggregate((long)1, (acc, v) => (long)acc * (long)v);
+                    }
+                    else if (bestCount == state.Item1.Count)
+                    {
+                        bestEntanglement = Math.Min(bestEntanglement, state.Item1.Aggregate((long)1, (acc, v) => (long)acc * (long)v));
+                    }
+                    continue;
+                }
+
+                if (state.Item1.Count >= bestCount)
+                {
+                    // Prune this path
+                    continue;
+                }
+
+                // Enqueue possibilities
+                int max = state.Item1.Count > 0 ? state.Item1.Last() : int.MaxValue;
+                foreach (int w in state.Item2.Where(g => g < max))
+                {
+                    queue.Enqueue((state.Item1.Append(w).ToList(), state.Item2.Where(g => g < w).ToList()));
+                }
+            }
+            return bestEntanglement;
         }
     }   
 }
