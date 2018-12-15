7e8b0041c6ec82762e09804a48f1dd1e8b9a2ff6
payou <alexandre.perrin@ubisoft.com>
2018-12-15 20:56:54 +0100

    Day 15 of 2018

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace Aoc
         static void Main(string[] args)
         {
             // Get the command line
-            string[] command = new string[3] { "execute", "2018", "15" };
+            string[] command = new string[3] { "execute", "2018", "16" };
             if (args.Length > 0)
             {
                 command = args;
