1c8b4e271a707050a4d731a79d1a1ed2bdac29ca
payou <alexandre.perrin@ubisoft.com>
2019-12-08 17:00:37 +0100

    Day 7 of 2019

---
+++ days/2019/Day201907.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+using Aoc.Common;
+using Aoc.Common.Simulators;
+
+namespace Aoc
+{
+    public class Day201907 : Aoc.Framework.Day
+    {
+        public string Codename { get; private set; }
+
+        public string Name { get; private set; }
+
+        private IntCpu _cpu;
+
+        private IntCpu[] _amplifiers;
+
+        private int[] _code;
+
+        public Day201907()
+        {
+            Codename = "2019-07";
+            Name = "Amplification Circuit";
+        }
+
+        public void Init()
+        {
+            _code = Aoc.Framework.Input.GetIntVector(this, ",");
+            _cpu = new IntCpu();
+            _amplifiers = new IntCpu[5];
+            for (int i = 0; i < 5; ++i)
+            {
+                _amplifiers[i] = new IntCpu();
+            }
+
+            _amplifiers[0].OnOutput += () => Pipe(0);
+            _amplifiers[1].OnOutput += () => Pipe(1);
+            _amplifiers[2].OnOutput += () => Pipe(2);
+            _amplifiers[3].OnOutput += () => Pipe(3);
+            _amplifiers[4].OnOutput += () => Pipe(4);
+        }
+
+        private void Pipe(int i)
+        {
+            _amplifiers[(i + 1) % 5].Input.Enqueue(_amplifiers[i].Output.Dequeue());
+        }
+
+        public string Run(Aoc.Framework.Part part)
+        {
+            if (part == Aoc.Framework.Part.Part1)
+            {
+                int max = TestAllSettings(DirectRun, 0, new List<int>(), new List<int> {0, 1, 2, 3, 4});
+                return max.ToString();
+            }
+
+            if (part == Aoc.Framework.Part.Part2)
+            {
+                int max = TestAllSettings(FeedbackRun, 0, new List<int>(), new List<int> {5, 6, 7, 8, 9});
+                return max.ToString();
+            }
+
+            return "";
+        }
+
+        private int TestAllSettings(Func<List<int>, int> evaluator, int max, List<int> used, List<int> available)
+        {
+            if (available.Count == 0)
+            {
+                // Test this one
+                return Math.Max(max, evaluator(used));
+            }
+
+            int output = max;
+            for (int i = 0; i < available.Count; ++i)
+            {
+                int phase = available[i];
+                used.Add(phase);
+                available.RemoveAt(i);
+                output = TestAllSettings(evaluator, output, used, available);
+                available.Insert(i, phase);
+                used.RemoveAt(used.Count - 1);
+            }
+
+            return output;
+        }
+
+        private int DirectRun(List<int> settings)
+        {
+            int output = 0;
+            for (int i = 0; i < settings.Count; ++i)
+            {
+                _cpu.Reset(_code);
+                _cpu.Input.Enqueue(settings[i]);
+                _cpu.Input.Enqueue(output);
+                _cpu.Run(true);
+                output = _cpu.Output.Dequeue();
+            }
+
+            return output;
+        }
+
+        private int FeedbackRun(List<int> settings)
+        {
+            
+            for (int i = 0; i < settings.Count; ++i)
+            {
+                _amplifiers[i].Reset(_code);
+                _amplifiers[i].Input.Enqueue(settings[i]);
+            }
+
+            _amplifiers[0].Input.Enqueue(0);
+            while (!_amplifiers[0].Halted)
+            {
+                for (int i = 0; i < settings.Count; ++i)
+                {
+                    _amplifiers[i].Run(true);
+                }
+            }
+
+            return _amplifiers[0].Input.Dequeue();
+        }
+    }   
+}
