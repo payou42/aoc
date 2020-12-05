b5de327738c5f296027a6c3186ea2ee50721a6d0
payou42 <alexandre.perrin@laposte.net>
2020-12-05 11:29:12 +0100

    Day 21 of 2018

--- days/2018/Day201821.cs
+++ days/2018/Day201821.cs
@@ -12,124 +12,50 @@ namespace Aoc
 
         public string Name { get; private set; }
 
-        public class Registers : IEquatable<Registers>
+        public enum OpCodes
         {
-            public int[] Values { get; set; }
-
-            public Registers()
-            {
-                Values = new int[6];
-            }
-
-            public Registers(Registers other)
-            {
-                Values = other.Values.ToArray();
-            }
-
-            public int this[int index]
-            {
-                get
-                {
-                    return Values[index];
-                }
-
-                set
-                {
-                    Values[index] = value;
-                }
-            }
-
-            public override bool Equals(Object other)
-            {
-                if ((other == null) || other.GetType() != typeof(Registers))
-                {
-                    return false;
-                }
-
-                return Equals((Registers)other);
-            }
-
-            public override int GetHashCode() => base.GetHashCode();
-
-            public bool Equals(Registers other)
-            {
-                if (other == null)
-                {
-                    return false;
-                }
-
-                for (int i = 0; i < 6; ++i)
-                {
-                    if (Values[i] != other.Values[i])
-                    {
-                        return false;
-                    }
-                }
-                
-                return true;
-            }
-
-            public static bool operator == (Registers r1, Registers r2)
-            {
-                if (((object)r1) == null || ((object)r2) == null)
-                    return Object.Equals(r1, r2);
-
-                return r1.Equals(r2);
-            }
-
-
-            public static bool operator != (Registers r1, Registers r2)
-            {
-                if (((object)r1) == null || ((object)r2) == null)
-                    return ! Object.Equals(r1, r2);
-
-                return !(r1.Equals(r2));
-            }
-        }
-
-        public class Command
-        {
-            public string Name { get; set; }
-
-            public Func<Registers, int, int, int> Executor { get; set; }
-
-            public Command(string name, Func<Registers, int, int, int> executor)
-            {
-                Name = name;
-                Executor = executor;
-            }
-
-            public Registers Execute(Registers before, Instruction i)
-            {
-                Registers after = new Registers(before);
-                after[i.C] = Executor(before, i.A, i.B);
-                return after;
-            }
+            Addr,
+            Addi,
+            Mulr,
+            Muli,
+            Banr,
+            Bani,
+            Borr,
+            Bori,
+            Setr,
+            Seti,
+            Gtir,
+            Gtri,
+            Gtrr,
+            Eqir,
+            Eqri,
+            Eqrr,
         }
 
         public class Instruction
         {
-            public string Opcode { get; set; }
+            public OpCodes Opcode { get; set; }
 
             public int A { get; set; }
 
             public int B { get; set; }
 
             public int C { get; set; }
+
+            public Action<int[], int, int, int> Action;
         }
 
-        private List<Instruction> _program;
+        private Instruction[] _program;
 
-        private Dictionary<string, Command> _commands;
+        private Dictionary<OpCodes, Action<int[], int, int, int>> _commands;
 
         private int _register;
-
 
         public class Cpu
         {
             public int Ip { get; set; }
 
-            public Registers Registers { get; set; }            
+            public int[] Registers { get; set; }            
         }
 
         public Day201821()
@@ -140,43 +66,29 @@ namespace Aoc
 
         public void Init()
         {
-            _commands = new Dictionary<string, Command>
+            _commands = new Dictionary<OpCodes, Action<int[], int, int, int>>
             {
-                { "addr", new Command("addr", (r, a, b) => r[a] + r[b]) },
-                { "addi", new Command("addi", (r, a, b) => r[a] + b) },
-                { "mulr", new Command("mulr", (r, a, b) => r[a] * r[b]) },
-                { "muli", new Command("muli", (r, a, b) => r[a] * b) },
-                { "banr", new Command("banr", (r, a, b) => r[a] & r[b]) },
-                { "bani", new Command("bani", (r, a, b) => r[a] & b) },
-                { "borr", new Command("borr", (r, a, b) => r[a] | r[b]) },
-                { "bori", new Command("bori", (r, a, b) => r[a] | b) },
-                { "setr", new Command("setr", (r, a, b) => r[a]) },
-                { "seti", new Command("seti", (r, a, b) => a) },
-                { "gtir", new Command("gtir", (r, a, b) => (a > r[b]) ? 1 : 0) },
-                { "gtri", new Command("gtri", (r, a, b) => (r[a] > b) ? 1 : 0) },
-                { "gtrr", new Command("gtrr", (r, a, b) => (r[a] > r[b]) ? 1 : 0) },
-                { "eqir", new Command("eqir", (r, a, b) => (a == r[b]) ? 1 : 0) },
-                { "eqri", new Command("eqri", (r, a, b) => (r[a] == b) ? 1 : 0) },
-                { "eqrr", new Command("eqrr", (r, a, b) => (r[a] == r[b]) ? 1 : 0) }
+                { OpCodes.Addr, (r, a, b, c) => r[c] = r[a] + r[b] },
+                { OpCodes.Addi, (r, a, b, c) => r[c] = r[a] + b },
+                { OpCodes.Mulr, (r, a, b, c) => r[c] = r[a] * r[b] },
+                { OpCodes.Muli, (r, a, b, c) => r[c] = r[a] * b },
+                { OpCodes.Banr, (r, a, b, c) => r[c] = r[a] & r[b] },
+                { OpCodes.Bani, (r, a, b, c) => r[c] = r[a] & b },
+                { OpCodes.Borr, (r, a, b, c) => r[c] = r[a] | r[b] },
+                { OpCodes.Bori, (r, a, b, c) => r[c] = r[a] | b },
+                { OpCodes.Setr, (r, a, b, c) => r[c] = r[a] },
+                { OpCodes.Seti, (r, a, b, c) => r[c] = a },
+                { OpCodes.Gtir, (r, a, b, c) => r[c] = (a > r[b]) ? 1 : 0 },
+                { OpCodes.Gtri, (r, a, b, c) => r[c] = (r[a] > b) ? 1 : 0 },
+                { OpCodes.Gtrr, (r, a, b, c) => r[c] = (r[a] > r[b]) ? 1 : 0 },
+                { OpCodes.Eqir, (r, a, b, c) => r[c] = (a == r[b]) ? 1 : 0 },
+                { OpCodes.Eqri, (r, a, b, c) => r[c] = (r[a] == b) ? 1 : 0 },
+                { OpCodes.Eqrr, (r, a, b, c) => r[c] = (r[a] == r[b]) ? 1 : 0 }
             };
 
-            _program = new List<Instruction>();
             string[] input = Aoc.Framework.Input.GetStringVector(this).Where(l => !string.IsNullOrEmpty(l)).ToArray();
-            int line = 0;
-            while (line < input.Length)
-            {
-                if (input[line].StartsWith("#ip"))
-                {
-                    _register = int.Parse(input[line].Substring(4));
-                    line++;
-                }
-                else
-                {
-                    Instruction instruction = ParseInstruction(input, line);
-                    _program.Add(instruction);
-                    line += 1;
-                }
-            }
+            _register = int.Parse(input[0].Substring(4));
+            _program = input.Skip(1).Select(line => ParseInstruction(line)).ToArray();
         }
 
         public string Run(Aoc.Framework.Part part)
@@ -184,7 +96,7 @@ namespace Aoc
             if (part == Aoc.Framework.Part.Part1)
             {
                 // Prepare the Cpu
-                Cpu cpu = new Cpu { Registers = new Registers(), Ip = 0};
+                Cpu cpu = new Cpu { Registers = new int[6], Ip = 0};
                     
                 // Run the program
                 while (true)
@@ -199,7 +111,7 @@ namespace Aoc
 
                     // Execute the instruction
                     Instruction instruction = _program[cpu.Ip];
-                    cpu.Registers = _commands[instruction.Opcode].Execute(cpu.Registers, instruction);
+                    instruction.Action(cpu.Registers, instruction.A, instruction.B, instruction.C);
 
                     // Copy back the register in the ip
                     cpu.Ip = cpu.Registers[_register] + 1;
@@ -209,7 +121,7 @@ namespace Aoc
             if (part == Aoc.Framework.Part.Part2)
             {
                 // prepare the Cpu
-                Cpu cpu = new Cpu { Registers = new Registers(), Ip = 0};
+                Cpu cpu = new Cpu { Registers = new int[6], Ip = 0};
 
                 // Values of R3
                 HashSet<int> history = new HashSet<int>();                
@@ -222,12 +134,14 @@ namespace Aoc
                     if (cpu.Ip == 28)
                     {
                         // Check r3
-                        if (history.Contains(cpu.Registers[3]))
+                        int r3 = cpu.Registers[3];
+                        if (history.Contains(r3))
                         {
                             return last.ToString();
-                        } 
-                        history.Add(cpu.Registers[3]);
-                        last = cpu.Registers[3];
+                        }
+
+                        history.Add(r3);
+                        last = r3;
                     }
 
                     // Copy the ip pointer in the register
@@ -235,7 +149,7 @@ namespace Aoc
 
                     // Execute the instruction
                     Instruction instruction = _program[cpu.Ip];
-                    cpu.Registers = _commands[instruction.Opcode].Execute(cpu.Registers, instruction);
+                    instruction.Action(cpu.Registers, instruction.A, instruction.B, instruction.C);
 
                     // Copy back the register in the ip
                     cpu.Ip = cpu.Registers[_register] + 1;
@@ -245,18 +159,22 @@ namespace Aoc
             return "";
         }
 
-        private Instruction ParseInstruction(string[] input, int line)
+        private Instruction ParseInstruction(string line)
         {
             // Parse an instruction like:
             // seti 0 2 1
-            string[] items = input[line].Split(" ");
+            string[] items = line.Split(" ");
+            OpCodes opcode;
+            Enum.TryParse(items[0], true, out opcode);
             Instruction instruction = new Instruction
             {
-                Opcode = items[0],
+                Opcode = opcode,
                 A = int.Parse(items[1]),
                 B = int.Parse(items[2]),
-                C = int.Parse(items[3])
+                C = int.Parse(items[3]),
+                Action = _commands[opcode],
             };
+
             return instruction;
         }
     }   
