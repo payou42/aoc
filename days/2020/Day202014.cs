e57328a127b99c6785d0434d4fc81ac760475397
payou42 <alexandre.perrin@laposte.net>
2020-12-14 08:31:06 +0100

    Day 14 of 2020

---
+++ days/2020/Day202014.cs
@@ -0,0 +1,142 @@
+using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+using Aoc.Common;
+
+namespace Aoc
+{
+    public class Day202014 : Aoc.Framework.IDay
+    {
+        public string Codename { get; private set; }
+
+        public string Name { get; private set; }
+
+        private string[] _input;
+
+        public Day202014()
+        {
+            Codename = "2020-14";
+            Name = "Docking Data";
+        }
+
+        public void Init()
+        {
+            _input = Aoc.Framework.Input.GetStringVector(this);
+        }
+
+        public string Run(Aoc.Framework.Part part)
+        {
+            if (part == Aoc.Framework.Part.Part1)
+            {
+                string mask = "";
+                Dictionary<long, long> mem = new Dictionary<long, long>();
+                foreach (string s in _input)
+                {
+                    // Process mask
+                    if (s.StartsWith("mask = "))
+                    {
+                        mask = s[7..];
+                        continue;
+                    }
+
+                    // Process mem storage
+                    string[] items = s.Split('[', ']');
+                    long addr = long.Parse(items[1]);
+                    long val = long.Parse(items[2][3..]);
+
+                    // Apply mask
+                    for (int i = 0; i < 36; ++i)
+                    {
+                        switch (mask[^(i + 1)])
+                        {
+                            case '0': val &= (~(1L << i) & ((1L << 36) - 1)); break;
+                            case '1': val |= (1L << i); break;
+                        }
+                    }
+
+                    // Store in memory
+                    mem[addr] = val;
+                }
+
+                return mem.Values.Sum().ToString();
+            }
+
+            if (part == Aoc.Framework.Part.Part2)
+            {
+                string mask = "";
+                Dictionary<long, long> mem = new Dictionary<long, long>();
+                foreach (string s in _input)
+                {
+                    // Process mask
+                    if (s.StartsWith("mask = "))
+                    {
+                        mask = s[7..];
+                        continue;
+                    }
+
+                    // Process mem storage
+                    string[] items = s.Split('[', ']');
+                    long addr = long.Parse(items[1]);
+                    long val = long.Parse(items[2][3..]);
+                    List<int> floating = new List<int>();
+
+                    // Apply mask
+                    for (int i = 0; i < 36; ++i)
+                    {
+                        switch (mask[^(i + 1)])
+                        {
+                            case 'X':
+                            {
+                                floating.Add(i);
+                                break;
+                            }
+                            
+                            case '1':
+                            {
+                                addr |= (1L << i);
+                                break;
+                            }
+                        }
+                    }
+
+                    // Store in memory
+                    this.WriteMemory(mem, addr, floating, val);
+                }
+
+                return mem.Values.Sum().ToString();
+            }
+
+            return "";
+        }
+
+        private void WriteMemory(Dictionary<long, long> mem, long addr, List<int> floating, long val)
+        {
+            Queue<(long, List<int>)> pendingWrites = new Queue<(long, List<int>)>();
+            pendingWrites.Enqueue((addr, floating));
+
+            while (pendingWrites.Count > 0)
+            {
+                var (a, l) = pendingWrites.Dequeue();
+                if (l.Count == 0)
+                {
+                    mem[a] = val;
+                }
+                else
+                {
+                    // Get the index to process
+                    int index = l.First();
+
+                    // Enqueue the 2 possible addresses
+                    long a1 = a |= 1L << index;
+                    var remainer1 = l.Skip(1).ToList();
+                    pendingWrites.Enqueue((a1, remainer1));
+
+                    long a2 = a & (~(1L << index) & ((1L << 36) - 1));
+                    var remainer2 = l.Skip(1).ToList();
+                    pendingWrites.Enqueue((a2, remainer2));
+                }
+            }
+        }
+    }   
+}
